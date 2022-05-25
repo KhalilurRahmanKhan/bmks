24c2f4fe50cda51488cc6eac1ee8c86114b0d51d
Khalilur Rahman Khan <khanjahid373@gmail.com>
2022-05-25 15:49:11 +0600

    add header

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,12 @@
+<div class="row header">
+    <div class="col-md-1 d-flex justify-content-center align-items-center">
+        <img src="{{ asset('images/logo.png') }}" alt="" class="header-logo">
+    </div>
+    <div class="col-md-9 d-flex justify-content-center align-items-center ">
+        <h1 class="font-weight-bold text-light">' বিধবা,স্বামী পরিত্যক্তা ও হতদরিদ্র জনগোষ্ঠীকে কর্মসংস্থানমূলক কাজের প্রশিক্ষণের মাধ্যমে আর্থসামাজিক অবস্থার উন্নয়ন ' প্রকল্প</h1>
+    </div>
+    <div class="col-md-2 d-flex justify-content-center align-items-center">
+        <img src="{{ asset('images/logo2.png') }}" alt="" class="header-logo me-3">
+        <img src="{{ asset('images/logo3.jpg') }}" alt="" class="header-logo">
+    </div>
+</div>
